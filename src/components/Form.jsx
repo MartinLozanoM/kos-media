dc077841d35a1d87ee243f6fffe04339f8d34f78
Martin Lozano Muñoz <martinlozanomu@gmail.com>
2023-11-06 22:09:42 -0300

    termine el formulario, y añadi datos de contacto en la seccion form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,11 +1,44 @@
-import { Image } from "react-bootstrap";
-import Logo from "../assets/kos-logo.jpeg";
+import { BsTelephone } from "react-icons/bs";
+import { GoMail } from "react-icons/go";
+import { FaInstagram, FaLinkedin } from "react-icons/fa";
 
 export const ContactForm = () => {
   return (
     <section className="container-fluid" id="contact-section">
-      <div className="row p-5 section-size">
-        <div className="bg-white  col-md-8 col-sm-12  p-4">
+      <div className="row p-3 section-size">
+        <div className="col-sm-12 col-md-6 text-center p-4 contact-text">
+          <div>
+            <h2>Mantengamonos en contacto</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
+              mollitia enim quia totam sint at quisquam minima illum nobis
+              facere, expedita quis? Accusantium vitae atque hic ducimus aut
+              corporis facere!
+            </p>
+          </div>
+          <div className="d-flex flex-column">
+            <span className="mb-4">
+              <BsTelephone size={30} className="me-3" />
+              <a href="https://api.whatsapp.com/send?phone=+543815156811&text=Hola%21+Quisiera+mas+informacion+sobre+el+HUB">
+                +54 9 381-5156811
+              </a>
+            </span>
+            <span className="mb-4">
+              <GoMail size={30} className="me-3" />
+              contacto@kosmedia.com.ar
+            </span>
+          </div>
+          <div className="">
+            <h3>Conectate con nosotros:</h3>
+            <a href="https://www.instagram.com" className="mx-2">
+              <FaInstagram size={36} />
+            </a>
+            <a href="https://www.linkedin.com/" className="mx-2">
+              <FaLinkedin size={36} />
+            </a>
+          </div>
+        </div>
+        <div className="bg-white col-sm-12 col-md-6 p-4">
           <div className="container">
             <h2 className="">Contactanos</h2>
             <form className="row my-4" name="contact" method="POST">
@@ -13,14 +46,14 @@ export const ContactForm = () => {
               <div className="row100">
                 <div className="column">
                   <div className="inputBox">
-                    <input type="text" name="" required />
+                    <input type="text" name="Nombre" required />
                     <span className="text">Nombre</span>
                     <span className="line"></span>
                   </div>
                 </div>
                 <div className="column">
                   <div className="inputBox">
-                    <input type="text" name="" required />
+                    <input type="text" name="Apellido" required />
                     <span className="text">Apellido</span>
                     <span className="line"></span>
                   </div>
@@ -29,14 +62,14 @@ export const ContactForm = () => {
               <div className="row100">
                 <div className="column">
                   <div className="inputBox">
-                    <input type="text" name="" required />
+                    <input type="text" name="Email" required />
                     <span className="text">Email</span>
                     <span className="line"></span>
                   </div>
                 </div>
                 <div className="column">
                   <div className="inputBox">
-                    <input type="text" name="" required />
+                    <input type="text" name="Telefono" required />
                     <span className="text">Telefono</span>
                     <span className="line"></span>
                   </div>
@@ -45,83 +78,19 @@ export const ContactForm = () => {
               <div className="row100">
                 <div className="column">
                   <div className="inputBox textarea">
-                    <textarea required></textarea>
+                    <textarea name="Comentarios" required></textarea>
                     <span className="text">Escribe tu mensaje aqui...</span>
                     <span className="line"></span>
                   </div>
                 </div>
               </div>
               <div className="row100">
-                <div className="column">
+                <div className="column text-center">
                   <input type="submit" value="Enviar" />
                 </div>
               </div>
-              {/* <input
-                type="text"
-                className="required"
-                name="name"
-                placeholder="Nombre"
-                style={{ paddingBottom: "35px", color: "black" }}
-              />
-              <input
-                type="text"
-                className="required"
-                name="lastname"
-                placeholder="Apellido"
-                style={{ paddingBottom: "35px", color: "black" }}
-              />
-              <input
-                type="email"
-                className="required"
-                name="email"
-                placeholder="Email"
-                style={{ paddingBottom: "35px", color: "black" }}
-              />
-              <input
-                type="text"
-                className="required"
-                name="telephone"
-                placeholder="Telefono"
-                style={{ paddingBottom: "35px", color: "black" }}
-              />
-              <input
-                type="text"
-                className="required"
-                name="company"
-                placeholder="Empresa"
-                style={{ paddingBottom: "35px", color: "black" }}
-              />
-              <textarea
-                type="text"
-                className="required"
-                name="comments"
-                placeholder="Comentarios"
-                style={{ paddingBottom: "35px", color: "black" }}
-              />
-              <div className="form-button-container col-12 pb-3">
-                <button className="" type="submit">
-                  Enviar
-                </button>
-              </div> */}
             </form>
           </div>
-        </div>
-        <div className="col-md-4 col-sm-12 d-flex flex-column justify-content-center align-item-center">
-          <h2
-            className="text-center "
-            style={{ marginTop: "10px", paddingTop: "10px" }}
-          >
-            Somos KOS Media
-          </h2>
-          <p className="text-center pb-0 mb-0 ">
-            "Te ayudamos a hacer crecer tu negocio"
-          </p>
-          <Image
-            src={Logo}
-            className="pt-0 mt-0 pb-3"
-            fluid
-            alt="Imagen del logo de la empresa"
-          ></Image>
         </div>
       </div>
     </section>
